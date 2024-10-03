338f0e3322a0117ff7c80e5dc79ee350928020cd
kinkriashvili <zhiend0331@gmail.com>
2024-10-03 18:10:44 +0400

    addd quizz images aandd qustions

---
+++ data/qustions.js
@@ -0,0 +1,104 @@
+export const quiz = [
+  {
+    qustion: "What are you looking for?",
+    answers: [
+      {
+        name: "Women's styles",
+        image: "#",
+      },
+      {
+        name: "Men's styles",
+        image: "#",
+      },
+    ],
+  },
+  {
+    qustion: "Want sunglasses recommendations too?",
+    answers: [
+      {
+        name: "Sunglasses",
+        image: "#",
+      },
+      {
+        name: "Eyeglasses",
+        image: "#",
+      },
+    ],
+  },
+  {
+    qustion: "What's your face width?",
+    answers: [
+      {
+        name: "Narrow",
+        image: "#",
+      },
+      {
+        name: "Medium",
+        image: "#",
+      },
+      {
+        name: "Wide",
+        image: "#",
+      },
+    ],
+  },
+  {
+    qustion: "Which shapes do you like?",
+    answers: [
+      {
+        name: "Round",
+        image: "#",
+      },
+      {
+        name: "Rectangle",
+        image: "#",
+      },
+      {
+        name: "Cat-eye",
+        image: "#",
+      },
+    ],
+  },
+  {
+    qustion: "Which colors do you like",
+    answers: [
+      {
+        name: "Black",
+        image: "#",
+      },
+      {
+        name: "Red",
+        image: "#",
+      },
+      {
+        name: "Golden",
+        image: "#",
+      },
+      {
+        name: "Silver",
+        image: "#",
+      },
+      {
+        name: "Blue",
+        image: "#",
+      },
+    ],
+  },
+  {
+    qustion: "Which materials do you like",
+    answers: [
+      {
+        name: "Acetate",
+        image: "#",
+      },
+      {
+        name: "Metal",
+        image: "#",
+      },
+      {
+        name: "Mixed",
+        image: "#",
+      },
+    ],
+  },
+];
--- scripts/loadPage/quizzLoad.js
+++ scripts/loadPage/quizzLoad.js
@@ -1,1 +1,9 @@
-console.log("me");
+import { loadHeader } from "./loadheader.js";
+import { loadFooter } from "./loadfooter.js";
+import { quiz } from "../../data/qustions.js";
+
+function loadQuiz() {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadHeader();
+});
