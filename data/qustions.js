18519931a1e65177ce34f396942fac47f244914c
kinkriashvili <zhiend0331@gmail.com>
2024-10-04 19:36:30 +0400

    ქუზების ჰტმლსა და ჯავასკრიპტის გაკეთება

--- data/qustions.js
+++ data/qustions.js
@@ -1,103 +1,137 @@
-export const quiz = [
+export const quizes = [
   {
+    qustionId: "1",
     qustion: "What are you looking for?",
     answers: [
       {
         name: "Women's styles",
-        image: "#",
+        image: "images/quiz/choosegenderwomen.avif",
+        answerId: "1-1",
       },
       {
         name: "Men's styles",
-        image: "#",
+        image: "images/quiz/choosegenderman.avif",
+        answerId: "1-2",
       },
     ],
   },
   {
+    qustionId: "2",
     qustion: "Want sunglasses recommendations too?",
     answers: [
       {
         name: "Sunglasses",
-        image: "#",
+        image: "images/quiz/choosesunglasses.avif",
+        answerId: "2-1",
       },
       {
         name: "Eyeglasses",
-        image: "#",
+        image: "images/quiz/chooseeyeglasses.avif",
+        answerId: "2-2",
       },
     ],
   },
   {
+    qustionId: "3",
     qustion: "What's your face width?",
     answers: [
       {
         name: "Narrow",
-        image: "#",
+        image: "images/quiz/choosewidth-narrow.avif",
+        answerId: "3-1",
       },
       {
         name: "Medium",
-        image: "#",
+        image: "images/quiz/choosewidth-medium.avif",
+        answerId: "3-2",
       },
       {
         name: "Wide",
-        image: "#",
+        image: "images/quiz/choosewidth-wide.avif",
+        answerId: "3-3",
       },
     ],
   },
   {
+    qustionId: "4",
     qustion: "Which shapes do you like?",
     answers: [
       {
         name: "Round",
-        image: "#",
+        image: "images/quiz/chooseshaperround.avif",
+        answerId: "4-1",
       },
       {
         name: "Rectangle",
-        image: "#",
+        image: "images/quiz/chooseshaperrectangle.avif",
+        answerId: "4-2",
+      },
+      {
+        name: "Square",
+        image: "images/quiz/choose-shape-square.avif",
+        answerId: "4-3",
       },
       {
         name: "Cat-eye",
-        image: "#",
+        image: "images/quiz/chooseshapercat-eye.avif",
+        answerId: "4-4",
+      },
+      {
+        name: "Aviator",
+        image: "images/quiz/choose-shape-aviator.avif",
+        answerId: "4-5",
       },
     ],
   },
   {
+    qustionId: "5",
     qustion: "Which colors do you like",
     answers: [
       {
         name: "Black",
         image: "#",
+        answerId: "5-1",
       },
       {
         name: "Red",
         image: "#",
+        answerId: "5-2",
       },
       {
-        name: "Golden",
+        name: "Goldenrod",
         image: "#",
+        answerId: "5-3",
       },
       {
         name: "Silver",
         image: "#",
+        answerId: "5-4",
       },
       {
         name: "Blue",
         image: "#",
+        answerId: "5-5",
       },
     ],
   },
   {
+    qustionId: "6",
     qustion: "Which materials do you like",
     answers: [
       {
         name: "Acetate",
-        image: "#",
+        image: "images/quiz/choosematerialacetate.avif",
+        answerId: "6-1",
       },
       {
         name: "Metal",
-        image: "#",
+        image: "images/quiz/choosematerialametal.avif",
+        answerId: "6-2",
       },
       {
         name: "Mixed",
-        image: "#",
+        image: "images/quiz/choosematerialamixed.avif",
+        answerId: "6-3",
       },
     ],
   },
--- scripts/loadPage/quizzLoad.js
+++ scripts/loadPage/quizzLoad.js
@@ -1,9 +1,85 @@
-import { loadHeader } from "./loadheader.js";
+import { loadHeader } from "./loadHeader.js";
 import { loadFooter } from "./loadfooter.js";
-import { quiz } from "../../data/qustions.js";
+import { quizes } from "../../data/qustions.js";
+import { loadProductsFetch } from "../../data/products.js";
 
-function loadQuiz() {}
+let managerIndex = 0;
+let qustionNumber = document.querySelector(".qustion-number");
+let quizzQustion = document.querySelector(".quizz-qustion");
+let quizAnswer = document.querySelector(".quizz-answers");
+
+function answerHml(quiz) {
+  let quizHtml = ``;
+  quiz.answers.forEach((answer) => {
+    if (quiz.qustionId != 5) {
+      quizHtml += `
+        <div class="quizAnswerContainer" answerId="${answer.answerId}">
+          <p>${answer.name}</p>
+          <img src="${answer.image}" alt="" />
+        </div>
+        `;
+    } else {
+      quizHtml += `
+        <div class="quizAnswerContainer" answerId="${answer.answerId}">
+          <p>${answer.name}</p>
+          <div style="background-color: ${answer.name.toLowerCase()}" class="answerOfColors"></div>
+        </div>
+        `;
+    }
+  });
+  quizAnswer.innerHTML = quizHtml;
+}
+
+function nextQustion() {
+  for (let i = 0; i < quizes.length; i++) {
+    let quiz = quizes[i];
+    if (Number(quiz.qustionId) == Number(qustionNumber.innerHTML) + 1) {
+      qustionNumber.innerHTML = quiz.qustionId;
+      quizzQustion.innerHTML = quiz.qustion;
+      answerHml(quiz);
+      attachAnswerClickEvent();
+      break;
+    }
+  }
+}
+function previousQustion() {
+  quizes.forEach((quiz, index) => {
+    if (Number(quiz.qustionId) == Number(qustionNumber.innerHTML) - 1) {
+      qustionNumber.innerHTML = quiz.qustionId;
+      quizzQustion.innerHTML = quiz.qustion;
+      answerHml(quiz);
+      attachAnswerClickEvent();
+    }
+  });
+}
+
+function managment() {
+  document.querySelectorAll(".quizz-management").forEach((manager) => {
+    manager.addEventListener("click", (event) => {
+      if (event.target.innerHTML == "next") {
+        nextQustion();
+      } else if (event.target.innerHTML == "previous") {
+        previousQustion();
+      }
+    });
+  });
+  attachAnswerClickEvent();
+}
+function attachAnswerClickEvent() {
+  document.querySelectorAll(".quizAnswerContainer").forEach((clickOnAnswer) => {
+    clickOnAnswer.addEventListener("click", () => {
+      console.log("we");
+      nextQustion();
+    });
+  });
+}
+
+async function loadQuiz() {
+  await loadProductsFetch();
+  managment();
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   loadHeader();
+  loadQuiz();
 });
